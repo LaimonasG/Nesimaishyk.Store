93fa7faaa7246fa4f254e89dee6dcb897e5ddbd2
Laimonas Gaučas <laimis.gaucas@gmail.com>
2023-09-18 12:46:26 +0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,26 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        primary: "#ed9987",
+        secondary: "#d22cf7",        
+        accent: "#e222a9",      
+        neutral: "#22242f",      
+        "base-100": "#ede7ee",        
+        info: "#a9c7e5",        
+        success: "#45e8b4",       
+        warning: "#d5a507",     
+        error: "#ef7684",
+        body:{
+          "background-color":"#cacccc"
+        }
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
+} 
 export default config
