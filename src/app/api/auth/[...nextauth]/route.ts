309993620e59e578d5837e40d1c1b7ee212fb258
Laimonas Gaučas <laimis.gaucas@gmail.com>
2023-09-22 12:00:28 +0300

    minor fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,13 @@ import { mergeAnynymousCartToUserCart } from "@/lib/db/cart";
 import { env } from "@/lib/env";
 import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions:NextAuthOptions ={
-  adapter:PrismaAdapter(prisma) as Adapter,
+  adapter:PrismaAdapter(prisma as PrismaClient) as Adapter,
    providers:[
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
